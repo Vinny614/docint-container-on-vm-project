1365e6f344fa3ffb1a1f91ff3f304395806fef51
Vinny614 <68601261+Vinny614@users.noreply.github.com>
2025-06-05 16:17:11 +0000

    Changed the ASP to Basic

--- infra/main.tf
+++ infra/main.tf
@@ -69,8 +69,8 @@ resource "azurerm_app_service_plan" "asp" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   sku {
-    tier = "Free"
-    size = "F1"
+    tier = "Basic"
+    size = "B1"
   }
 }
 
