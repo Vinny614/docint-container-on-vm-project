413854f2735fc90954df42bdac8b51db1acc4c78
Vinny614 <68601261+Vinny614@users.noreply.github.com>
2025-06-06 10:12:08 +0000

    Changed to the linux web app from the app service

--- infra/main.tf
+++ infra/main.tf
@@ -74,14 +74,16 @@ resource "azurerm_app_service_plan" "asp" {
   }
 }
 
-resource "azurerm_app_service" "app" {
+resource "azurerm_linux_web_app" "app" {
   name                = var.app_service_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.asp.id
+  service_plan_id     = azurerm_app_service_plan.asp.id
 
   site_config {
-    linux_fx_version = "PYTHON|3.11"
+    application_stack {
+      python_version = "3.11"
+    }
   }
 
   app_settings = {
